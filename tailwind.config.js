441e70cc5082972b9a906e65c9a9af6d13580066
ensuidev <contactovenetrex@gmail.com>
2023-02-26 11:39:23 -0500

    Creation of the navbar, main inf. And centering

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+        },
+    },
+    plugins: [],
+}
